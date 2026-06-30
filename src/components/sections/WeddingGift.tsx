8739368a2a176223378b72077ce7d4fbbca7439e
Riqza AF <maulana.10125908@mahasiswa.unikom.ac.id>
2026-06-30 23:39:27 +0700

    animasi wedding gift

--- src/components/sections/WeddingGift.tsx
+++ src/components/sections/WeddingGift.tsx
@@ -25,11 +25,7 @@ const WeddingGift = () => {
           className="object-cover block lg:object-bottom"
         />
         <motion.h2
-          variants={fadeUp}
-          initial="hidden"
-          whileInView="show"
-          viewport={{ once: true, amount: 0.3 }}
-          transition={{ duration: 3, ease: "easeOut" }}
+         
           className="absolute -bottom-[2.31vw] left-0 font-montreal font-regular text-[17.95vw] lg:text-[18vw] whitespace-nowrap text-[#F2F1E8] leading-none"
         >
           Wedding Gift
